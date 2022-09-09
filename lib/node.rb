f327ea8cf48ebe190288114e522a162ece3a0b82
Nicole Leinich <nicole.leinich@ivx.com>
2022-09-09 15:42:59 +0200

    include comparable module + compare methods

--- lib/node.rb
+++ lib/node.rb
@@ -1,7 +1,17 @@
 class Node
+  include Comparable
+  attr_reader :value
   def initialize(value, left_child = nil, right_child = nil)
     @value = value
     @left_child = left_child
     @right_child = right_child
+  end
+
+  def is_greater?(node)
+    value > node.value
+  end
+
+  def is_smaller?(node)
+    value < node.value
   end
 end
