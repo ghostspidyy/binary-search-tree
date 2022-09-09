0960d5227278ae200a2b263da00839d3031c6056
Nicole Leinich <nicole.leinich@ivx.com>
2022-09-09 15:51:21 +0200

    add #pretty_print + return for root

--- lib/tree.rb
+++ lib/tree.rb
@@ -6,5 +6,13 @@ class Tree
 
   def build_tree(arr)
     arr.sort.uniq
+
+    return arr[arr.length/2]
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
