37c423af83d650d021f1339afe76045568b8e86e
Nicole Leinich <nicole.leinich@ivx.com>
2022-09-12 16:40:16 +0200

    Introduced #insert(value)

--- lib/tree.rb
+++ lib/tree.rb
@@ -3,6 +3,7 @@ class Tree
   def initialize(arr)
     @arr = arr
     @root = build_tree(arr.sort.uniq)
+    insert(2)
     pretty_print
   end
 
@@ -24,6 +25,29 @@ class Tree
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+  def insert(value)
+    node = Node.new(value)
+    comparable_node = @root
+    good_to_go = false
+    until good_to_go
+      if node.is_greater?(comparable_node)
+        if comparable_node.right.nil?
+          good_to_go = true
+          comparable_node.right = node
+        else
+          comparable_node = comparable_node.right
+        end
+      else
+        if comparable_node.left.nil?
+          good_to_go = true
+          comparable_node.left = node
+        else
+          comparable_node = comparable_node.left
+        end
+      end
+    end
   end
 end
 
